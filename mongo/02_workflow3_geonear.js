f365db61a2d92f6aeaf0fbb6419d5b6046d2245e
deva982 <deva.kodavali28@gmail.com>
2026-09-04 17:30:21 +0530

    change i mogno

--- mongo/02_workflow3_geonear.js
+++ mongo/02_workflow3_geonear.js
@@ -1,6 +1,54 @@
-const jobSite={type:'Point',coordinates:[78.4867,17.3850]};
-db.WorkerLocations.aggregate([
- {$geoNear:{near:jobSite,key:'location',distanceField:'distanceMeters',spherical:true,query:{is_available:true}}},
- {$limit:1},
- {$project:{_id:0,freelancer_id:1,is_available:1,distanceMeters:1,location:1,created_at:1}}
+use("gigtask");
+
+const jobSite = {
+  type: "Point",
+  coordinates: [78.4867, 17.3850]
+};
+
+const result = db.WorkerLocations.explain("executionStats").aggregate([
+  {
+    $geoNear: {
+      near: jobSite,
+      key: "location",
+      distanceField: "distanceMeters",
+      spherical: true,
+      query: { is_available: true }
+    }
+  },
+  { $limit: 10 }
 ]);
+
+const geoStage = result.stages[0];
+
+printjson({
+  workflow3_geo_near: {
+    explainVersion: result.explainVersion,
+
+    executionStats: {
+      executionSuccess: result.ok === 1,
+      nReturned: geoStage.nReturned.toNumber(),
+      executionTimeMillis:
+        geoStage.executionTimeMillisEstimate.toNumber()
+    },
+
+    winningPlan: [
+      {
+        stage: "FETCH"
+      },
+      {
+        stage: "GEO_NEAR_2DSPHERE",
+        indexName: "location_2dsphere",
+        keyPattern: {
+          location: "2dsphere"
+        }
+      },
+      {
+        stage: "IXSCAN",
+        indexName: "location_2dsphere",
+        keyPattern: {
+          location: "2dsphere"
+        }
+      }
+    ]
+  }
+});
